38bf62595032a74e7dc3e5e61db9ba7bf7ae503f
Mike Ruiz <mikerzdev@gmail.com>
2021-08-27 12:04:01 -0500

    dialog fix

--- lib/ui/dialogs/dialog_widget.dart
+++ lib/ui/dialogs/dialog_widget.dart
@@ -4,39 +4,36 @@ Future<T?> showInformationDialog<T>(
   BuildContext context, {
   required String title,
   required String message,
+  bool isTwoButton = false,
   String okButtonText = 'Ok',
   VoidCallback? onOkPressed,
 }) =>
     showDialog<T>(
-      barrierColor: Colors.white,
       barrierDismissible: true,
       context: context,
       builder: (_) => AlertDialog(
         content: Column(
           mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Text(title),
             SizedBox(height: 20),
             Text(message),
-            SizedBox(height: 10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  child: Text('No'),
-                  onPressed: () => Navigator.pop(context),
-                ),
-                TextButton(
-                  child: Text(okButtonText),
-                  onPressed: () {
-                    Navigator.pop(context);
-                    onOkPressed?.call();
-                  },
-                ),
-              ],
-            )
           ],
         ),
+        actions: [
+          if (isTwoButton)
+            TextButton(
+              child: Text('No'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          TextButton(
+            child: Text(okButtonText),
+            onPressed: () {
+              Navigator.pop(context);
+              onOkPressed?.call();
+            },
+          ),
+        ],
       ),
     );
