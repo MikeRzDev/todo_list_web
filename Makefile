24a48b154a6629261413d7cdf7270c6494f1f0da
Mike Ruiz <miguel.ruiz>
2021-10-06 16:06:22 -0500

    ios config

---
+++ Makefile
@@ -0,0 +1,14 @@
+generate:
+	flutter packages pub run build_runner build --delete-conflicting-outputs
+
+watch:
+	flutter packages pub run build_runner watch --delete-conflicting-outputs
+
+app-ios:
+	flutter build ios --release
+
+app-android:
+	flutter build apk  
+
+clean-rebuild:
+	flutter clean && flutter packages get
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let googleMapsApiKey = Bundle.main.infoDictionary?["GOOGLE_MAPS_API_KEY"] as! String
+    GMSServices.provideAPIKey(googleMapsApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
